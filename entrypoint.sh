e80a1bc4e21ee97d11877b05dc462a2f24cb457f
Aliaksandr <alexander.dovguchits@gmail.com>
2023-05-03 21:42:45 +0200

    the commands have been swapped out

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-poetry run python authentication/manage.py runserver 0.0.0.0:8000
 poetry run python authentication/manage.py migrate
+poetry run python authentication/manage.py runserver 0.0.0.0:8000
