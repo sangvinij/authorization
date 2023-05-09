3f8d896d94fa05cf80203dfcf6a81b0c6ab4adeb
Artyom Vorozhbyanov <artyom.vorozhbyanov@gmail.com>
2023-05-09 17:26:02 +0300

    LS characters saved for bash script, admin panel modified

--- authentication/accounts/admin.py
+++ authentication/accounts/admin.py
@@ -1,6 +1,55 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth.password_validation import validate_password
 
 from .models import User
 
 
-admin.site.register(User)
+class UserCreationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name')
+
+    def clean_password(self):
+        password = self.cleaned_data.get("password")
+        validate_password(password)
+        return password
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    list_display = ['pk', 'email', 'first_name', 'last_name']
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email', )
+    fieldsets = ()
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password'),
+        }),
+    )
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
